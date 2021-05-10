0fe613a0b0cb78ac883046cc781360687075c2ec
codetasker007 <83946048+codetasker007@users.noreply.github.com>
2021-05-10 13:23:28 +0530

    final

---
+++ js.js
@@ -0,0 +1,87 @@
+function openCity(cityName) {
+    var i;
+    var x = document.getElementsByClassName("city");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    document.getElementById(cityName).style.display = "block";  
+      
+  }
+
+
+
+
+
+  $(document).ready(function(){
+    $('.fade').slick({
+      dots: false,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear',
+      // autoplay: true,
+      // autoplaySpeed: 2000
+    });
+
+
+
+
+
+
+    
+    $('.center-slider').slick({
+      centerMode: true,
+      centerPadding: '200px',
+      slidesToShow:2,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+
+
+    $('.center').slick({
+      centerMode: true,
+      centerPadding: '60px',
+      slidesToShow: 3,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+
+    
+  });
+  // Input Lock
